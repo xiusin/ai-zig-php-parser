dcc14f0c4ec8c1838578917ecf2505106c75891e
xiusin <826466266@qq.com>
2026-03-24 12:57:32 +0800

    aa

--- zig-php-parser/src/aot/runtime_lib_template.zig
+++ zig-php-parser/src/aot/runtime_lib_template.zig
@@ -7874,18 +7874,18 @@ pub fn php_strnatcasecmp(str1: Value, str2: Value) !Value {
 /// 自然排序比较算法
 /// 将数字部分作为整数比较，非数字部分按字符比较
 fn naturalCompare(s1: []const u8, s2: []const u8, case_insensitive: bool) i64 {
-    var i1: usize = 0;
-    var i2: usize = 0;
+    var idx1: usize = 0;
+    var idx2: usize = 0;
 
-    while (i1 < s1.len and i2 < s2.len) {
-        const c1 = s1[i1];
-        const c2 = s2[i2];
+    while (idx1 < s1.len and idx2 < s2.len) {
+        const c1 = s1[idx1];
+        const c2 = s2[idx2];
 
         // 检查是否都是数字
         if (std.ascii.isDigit(c1) and std.ascii.isDigit(c2)) {
             // 跳过前导零
-            while (i1 < s1.len and s1[i1] == '0') i1 += 1;
-            while (i2 < s2.len and s2[i2] == '0') i2 += 1;
+            while (idx1 < s1.len and s1[idx1] == '0') idx1 += 1;
+            while (idx2 < s2.len and s2[idx2] == '0') idx2 += 1;
 
             // 提取数字
             var num1: i64 = 0;
@@ -7893,13 +7893,13 @@ fn naturalCompare(s1: []const u8, s2: []const u8, case_insensitive: bool) i64 {
             var len1: usize = 0;
             var len2: usize = 0;
 
-            while (i1 + len1 < s1.len and std.ascii.isDigit(s1[i1 + len1])) {
-                num1 = num1 * 10 + (s1[i1 + len1] - '0');
+            while (idx1 + len1 < s1.len and std.ascii.isDigit(s1[idx1 + len1])) {
+                num1 = num1 * 10 + (s1[idx1 + len1] - '0');
                 len1 += 1;
             }
 
-            while (i2 + len2 < s2.len and std.ascii.isDigit(s2[i2 + len2])) {
-                num2 = num2 * 10 + (s2[i2 + len2] - '0');
+            while (idx2 + len2 < s2.len and std.ascii.isDigit(s2[idx2 + len2])) {
+                num2 = num2 * 10 + (s2[idx2 + len2] - '0');
                 len2 += 1;
             }
 
@@ -7909,8 +7909,8 @@ fn naturalCompare(s1: []const u8, s2: []const u8, case_insensitive: bool) i64 {
             }
 
             // 数字相同，继续比较
-            i1 += len1;
-            i2 += len2;
+            idx1 += len1;
+            idx2 += len2;
         } else {
             // 非数字部分，按字符比较
             const ch1 = if (case_insensitive) std.ascii.toLower(c1) else c1;
@@ -7920,14 +7920,14 @@ fn naturalCompare(s1: []const u8, s2: []const u8, case_insensitive: bool) i64 {
                 return if (ch1 < ch2) -1 else 1;
             }
 
-            i1 += 1;
-            i2 += 1;
+            idx1 += 1;
+            idx2 += 1;
         }
     }
 
     // 长度不同
-    if (i1 < s1.len) return 1;
-    if (i2 < s2.len) return -1;
+    if (idx1 < s1.len) return 1;
+    if (idx2 < s2.len) return -1;
     return 0;
 }
 
